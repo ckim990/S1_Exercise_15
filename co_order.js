ab2b4a86e7e460d52564ec91a17727ee4e2f7b3d
Chris Kim <ckim990@west-mec.org>
2019-04-18 14:29:10 -0700

    Final Commit Ready for Review

--- co_order.js
+++ co_order.js
@@ -29,26 +29,75 @@
 */
 
 window.addEventListener("load", function () {
-      var orderForm = document.forms.orderForm 
+
+      var orderForm = document.forms.orderForm;
       orderForm.elements.orderDate.value = new Date().toDateString();
       orderForm.elements.model.focus();
+
+      // Calculate the cost of the order.
       calcOrder();
+
+      // Event handlers for the web form.
+      orderForm.elements.model.onchange = calcOrder;
+      orderForm.elements.qty.onchange = calcOrder;
+
+      var planOptions = document.querySelectorAll('input[name="protection"]');
+      for (var i = 0; i < planOptions.length; i++) {
+            planOptions[i].onclick = calcOrder;
+      };
+
 });
 
 function calcOrder() {
+
       var orderForm = document.forms.orderForm;
 
+      // Calculate the intial cost of the order. 
       var mIndex = orderForm.elements.model.selectedIndex;
       var mCost = orderForm.elements.model.options[mIndex].value;
       var qIndex = orderForm.elements.qty.selectedIndex;
       var quantity = orderForm.elements.qty[qIndex].value;
 
-      var initialCost = mCost * quantity;
-      orderForm.elements.initialCost.value = initialCost;
+      // Initial cost = model cosst * quantity.
+      var InitialCost = mCost * quantity;
+      orderForm.elements.initialCost.value = formatUSACurrency(InitialCost);
 
-      var pCost = document.querySelector('input[name="protection"]:checked')
-      .value * quantity;
-      orderForm.elements.protectionCost.value = pCost;
+      // Retrieve the cost of the user's protect 
+      var pCost = document.querySelector('input[name="protection"]:checked').value * quantity;
+      orderForm.elements.protectionCost.value = formatNumber(InitialCost + pCost, 2);
+
+      // Calculate the order subtotal.
+      orderForm.elements.subtotal.value = InitialCost + pCost;
+
+      // Calculate the sales tax.
+      var salesTax = 0.05 * (InitialCost + pCost);
+      orderForm.elements.salesTax.value = formatNumber(salesTax, 2);
+
+      // Calculate the cost of the total order.
+      var totalCost = InitialCost + pCost + salesTax;
+      orderForm.elements.totalCost.value = formatUSACurrency(totalCost);
+
+      // Store the order details.
+      orderForm.elements.modelName.value = orderForm.elements.model.options[mIndex].text;
+
+      orderForm.elements.protectionName.value = document.querySelector('input[name="protection"]:checked').nextSibling.nodeValue;
+
 }
 
+function formatNumber(val, decimals) {
 
+      return val.toLocaleString(undefined, {
+            minimumFractionDigits: decimals,
+            maximumFractionDigits: decimals
+
+      });
+}
+
+function formatUSACurrency(val) {
+
+      return val.toLocaleString('en-us', {
+            style: "currency",
+            currency: "USD"
+      });
+
+}
--- co_payment.js
+++ co_payment.js
@@ -44,3 +44,188 @@
 
 */
 
+window.addEventListener("load", function () {
+
+      // Retrieve the field/value pairs from the URL.
+      var formData = location.search.slice(1);
+      formData = formData.replace(/\+/g, " ");
+      formData = decodeURIComponent(formData);
+      var formField = formData.split(/[&=]/g);
+
+      // Write the field values to the order form.
+      document.forms.order.elements.orderDate.value = formField[1];
+      document.forms.order.elements.modelName.value = formField[5];
+      document.forms.order.elements.qty.value = formField[7];
+      document.forms.order.elements.protectionName.value = formField[9];
+      document.forms.order.elements.protectionCost.value = formField[13];
+      document.forms.order.elements.subtotal.value = formField[15];
+      document.forms.order.elements.salesTax.value = formField[19];
+      document.forms.order.elements.totalCost.value = formField[21];
+});
+
+window.addEventListener("load", function () {
+
+      document.getElementById("subButton").onclick = runSubmit;
+      document.getElementById("cardName").oninput = validateName;
+      document.getElementById("expMonth").onchange = validateMonth;
+      document.getElementById("expYear").onchange = validateYear;
+      document.getElementById("cvc").oninput = validateCVC;
+
+
+});
+
+function runSubmit() {
+
+      validateName();
+      validateCredit();
+      validateNumber();
+      validateMonth();
+      validateYear();
+      validateCVC();
+
+}
+
+function validateCVC() {
+
+      var cardCVC = document.getElementById("cvc");
+
+      var creditCard = document.querySelector("input[name='credit']:checked").value;
+
+      if (cardCVC.validity.valueMissing) {
+
+            cardCVC.setCustomValidity("Enter your CVC number");
+
+      } else if ((creditCard === "amex") && (/^\d{4}$/.test(cardCVC.value) === false)) {
+
+            cardCVC.setCustomValidity("Enter a   4-digit CVC number");
+
+      } else if ((creditCard === "amex") && (/^\d{3}$/.test(cardCVC.value) === false)) {
+
+            cardCVC.setCustomValidity("Enter a 3-digit CVC number");
+
+      } else {
+            cardCVC.setCustomValidity("");
+
+      }
+
+}
+
+function validateMonth() {
+
+      var cardMonth = document.getElementById("expMonth");
+
+      if (cardMonth.selectedIndex === 0) {
+
+            cardMonth.setCustomValidity("Select the expiration month");
+
+      } else {
+
+            cardMonth.setCustomValidity("");
+
+      }
+
+}
+
+function validateMonth() {
+
+      var cardYear = document.getElementById("expYear");
+
+      if (cardYear.selectedIndex === 0) {
+
+            cardYear.setCustomValidity("Select the expiration year");
+
+      } else {
+
+            cardYear.setCustomValidity("");
+
+      }
+
+}
+
+
+
+function validateNumber() {
+
+      var cardNumber = document.getElementById("cardNumber");
+
+      if (cardNumber.validity.valueMissing) {
+
+            cardNumber.setCustomValidity("Enter your card number");
+
+      } else if (cardNumber.validity.patternMismatch) {
+
+            cardNumber.setCustomValidity("Enter a valid card number");
+
+      } else if (luhn(cardNumber.value) === false) {
+
+            cardNumber.setCustomValidity("Enter a legitimate card number");
+
+      } else {
+
+            cardNumber.setCustomValidity("");
+
+      }
+
+}
+
+function validateCredit() {
+
+      var creditCard = document.forms.payment.elements.credit[0];
+
+      if (creditCard.validity.valueMissing) {
+
+            creditCard.setCustomValidity("Select your credit card");
+
+      } else {
+
+            creditCard.setCustomValidity("");
+
+      }
+
+}
+
+
+function validateName() {
+
+      var cardName = document.getElementById("cardName");
+
+      if (cardName.validity.valueMissing) {
+
+            cardName.setCustomValidity("enter your name as it appears on your card");
+
+      } else {
+            cardName.setCustomValidity("");
+      }
+
+}
+
+function sumDigits(numStr) {
+
+      var digitTotal = 0;
+      for (var i = 0; i < numStr.length; i++) {
+
+            digitTotal += parseInt(numStr.charAt(i));
+
+      }
+
+      return digitTotal;
+
+};
+
+
+function luhn(idNum) {
+
+      var string1 = "";
+      var string2 = "";
+
+      for (var i = idNum.length - 1; i >= 0; i -= 2) {
+            string1 += idNum.charAt(i);
+      };
+
+      for (var i = idNum.length - 2; i >= 0; i -= 2) {
+            string1 += 2 * idNum.charAt(i);
+      };
+
+
+      return sumDigits(string1 + string2) % 10 === 0;
+}
